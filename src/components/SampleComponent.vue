2506ca77fd32bf4f7eff0d275dbaace27c27fdfb
Andy Rothwell <rothwell.andy@gmail.com>
2020-12-01 09:42:06 -0500

    makes checkboxes work, changes .github workflow for push to S3

--- src/components/SampleComponent.vue
+++ src/components/SampleComponent.vue
@@ -1,10 +1,118 @@
 <template>
-  <div class="columns">
-    <div class="column" />
+  <div class="columns is-multiline is-centered">
+    <div class="column is-4">
+      <form>
+        <h3 class="title is-5 form-padding">
+          Form Inputs (no nesting)
+        </h3>
+        <textbox
+          v-model="textbox"
+          placeholder="A Textbox input"
+          :required="validationSettings.includes('required') ? true : undefined"
+          :error="validationSettings.includes('errors') ? 'this field is invalid' : ''"
+        />
+        <dropdown
+          v-model="dropdown"
+          :options="dropdownOptions"
+          :required="validationSettings.includes('required') ? true : undefined"
+          :error="validationSettings.includes('errors') ? 'this field is invalid' : ''"
+          placeholder="A dropdown field"
+        />
+        <div class="is-field-info-important">
+          Some importnant information about this field.
+        </div>
+        <Checkbox
+          v-model="Checkbox"
+          :options="CheckboxOptions"
+          :required="validationSettings.includes('required') ? true : undefined"
+          :error="validationSettings.includes('errors') ? 'this field is invalid' : ''"
+          label="A checkbox group"
+        />
+        <radio
+          v-model="radio"
+          :options="radioOptions"
+          :required="validationSettings.includes('required') ? true : undefined"
+          :error="validationSettings.includes('errors') ? 'this field is invalid' : ''"
+          label="A radio group"
+        />
+        <div class="is-field-info">
+          Some information about this field.
+        </div>
+        <text-area
+          v-model="textarea"
+          :required="validationSettings.includes('required') ? true : undefined"
+          :error="validationSettings.includes('errors') ? 'this field is invalid' : ''"
+          placeholder="A Text-area field"
+        />
+        <div class="form-submit form-padding has-text-centered">
+          <button>Submit</button>
+        </div>
+      </form>
+    </div>
+    <div class="column is-4">
+      <Checkbox
+        v-model="validationSettings"
+        :options="settings"
+        text-key="label"
+        value-key="key"
+      />
+      <h4 class="is-4">
+        Field values
+      </h4>
+      <b>textbox</b>: {{ textbox }}<br>
+      <b>dropdown</b>: {{ dropdown }}<br>
+      <b>checkbox</b>: {{ Checkbox }}<br>
+      <b>radio</b>: {{ radio }}<br>
+      <b>textarea</b>: {{ textarea }}<br>
+    </div>
   </div>
 </template>
 <script>
+
+import { Checkbox } from '@phila/phila-ui';
+console.log('checkbox:', Checkbox);
+
 export default {
-  name: "SampleComponent",
+  name: 'SampleComponent',
+  components: {
+    Checkbox,
+  },
+  data () {
+    return {
+      validationSettings: [],
+      settings: [
+        {
+          label: "Make all required",
+          key: "required",
+        },
+        {
+          label: "Show errors",
+          key: "errors",
+        },
+      ],
+      textbox: "",
+      dropdown: "",
+      dropdownOptions: [
+        "Option 1",
+        "Option 2",
+        "Option 3",
+      ],
+      Checkbox: [],
+      CheckboxOptions: [
+        "Option 1",
+        "Option 2",
+        "Option 3",
+      ],
+      radio: "",
+      radioOptions: [
+        "Option 1",
+        "Option 2",
+        "Option 3",
+      ],
+      textarea: "",
+    };
+  },
+  methods: {
+  },
 };
 </script>
