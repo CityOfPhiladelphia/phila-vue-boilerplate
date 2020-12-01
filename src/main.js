c0537ed01aa48e0496a36115b281f6c12f28d588
Daniel Faria <daniel.faria@phila.gov>
2020-12-01 09:41:44 -0500

    demo changes

--- src/main.js
+++ src/main.js
@@ -2,12 +2,26 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import { AppHeader, AppFooter, ResponsiveHelper } from '@phila/phila-ui';
+import * as PhilaUI from '@phila/phila-ui';
+import VueI18n from 'vue-i18n';
 
+Vue.use(VueI18n);
+
+Vue.use(PhilaUI);
 Vue.use(ResponsiveHelper);
 Vue.use(AppHeader);
 Vue.use(AppFooter);
 
+const i18n = new VueI18n({
+  locale: 'en-US',
+  messages: {
+    'en-US': {
+    },
+  },
+});
+
 new Vue({
   router,
+  i18n,
   render: h => h(App),
 }).$mount('#app');
