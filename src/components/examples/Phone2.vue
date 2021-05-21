635bc0ee1c2aa60f6cd0ea71678a683fa64b86ca
Daniel Faria <daniel.faria@phila.gov>
2021-05-21 16:06:35 -0400

    phone number required

--- src/components/examples/Phone2.vue
+++ src/components/examples/Phone2.vue
@@ -4,7 +4,7 @@
       <div class="column is-half">
         <vee-phone
           v-model="phoneNumber"
-          rules="phone"
+          rules="phone|required"
           name="Phone number"
         />
       </div>
@@ -14,14 +14,18 @@
 
 <script>
 import { Phone } from '@phila/phila-ui';
+import { required } from 'vee-validate/dist/rules';
 import { extend, withValidation, setInteractionMode } from 'vee-validate';
 
 setInteractionMode('aggressive');
 
 const VeePhone = withValidation(Phone);
+
 extend('phone', value => {
   return /\(\d{3}\) \d{3} - \d{4}/gi.test(value);
 });
+
+extend('required', required);
 
 export default {
   components: {
