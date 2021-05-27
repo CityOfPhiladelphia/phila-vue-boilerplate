8abd1bc6b7ef2e96241ecf85b99303f59422e7aa
Daniel Faria <daniel.faria@phila.gov>
2021-05-27 15:08:02 -0400

    adds bulma grid

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,335 @@
 <template>
   <div id="app">
     <app-header
-      app-title="Demo title"
+      app-title="Sample styles"
       :is-sticky="false"
     />
     <main>
       <div class="container">
-        Demo code starts here
+        <div class="columns">
+          <div class="column is-two-thirds">
+            <div class="content">
+              <div>
+                <code>
+                  title size 1 with large paragraph
+                </code>
+                <h1 class="title is-1">
+                  Engineering consultations
+                </h1>
+                <p class="is-large">
+                  Deflector power at maximum. <a href="#">Energy discharge</a> in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies. Resistance is futile. Recommend we adjust shield harmonics to the upper EM band when proceeding. These appear to be some kind of <a
+                    href="#"
+                    class="external-link"
+                  >power-wave-guide</a> conduits which allow them to work collectively as they perform ship functions. Increase deflector modulation to upper frequency band.
+                </p>
+              </div>
+              <hr>
+              <code>
+                title size 2 with medium paragraph (default paragraph size)
+              </code>
+              <h2 class="title is-2">
+                Rhomboid Dronegar 006
+              </h2>
+              <p class="is-medium">
+                Shields up. I recommend we <a href="#">transfer power</a> to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. <a
+                  href="#"
+                  class="external-link"
+                >Captain</a>, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.
+              </p>
+              <hr>
+              <code>
+                title size 3 with small paragraph
+              </code>
+              <h3 class="title is-3">
+                The shuttle has no power
+              </h3>
+              <p class="is-small">
+                It indicates a <a href="#">synchronic distortion</a> in the areas emanating triolic waves. The cerebellum, the cerebral cortex, the brain stem,  the entire nervous system has been depleted of electrochemical energy. Any device like that would produce high levels of triolic waves. These walls have undergone some kind of selective molecular polarization. I haven't determined if our <a
+                  href="#"
+                  class="external-link"
+                >phaser energy</a> can generate a stable field. We could alter the photons with phase discriminators.
+              </p>
+              <hr>
+              <code>
+                title size 4 with small paragraph
+              </code>
+              <h4 class="title is-4">
+                EM band when proceeding
+              </h4>
+              <p class="is-small">
+                Communication is not possible. The <a href="#">shuttle</a> has no power. Using the gravitational pull of a star to slingshot back in time? We are going to Starbase Montgomery for Engineering consultations prompted by minor read-out anomalies. Probes have recorded unusual levels of <a
+                  href="#"
+                  class="external-link"
+                >geological activity</a> in all five planetary systems. Assemble a team. Look at records of the Drema quadrant. Would these scans detect artificial transmissions as well as natural signals?
+              </p>
+              <hr>
+              <code>Pairing a title with a subtitle</code>
+              <hr>
+              <code>
+                title and subtitle size 1, with large paragraph
+              </code>
+              <h1 class="title is-1">
+                Engineering consultations
+              </h1>
+              <div class="subtitle is-1">
+                Something strange on the detector circuit
+              </div>
+              <p class="is-large">
+                Deflector power at maximum. Energy discharge in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies. Resistance is futile. Recommend we adjust shield harmonics to the upper EM band when proceeding. These appear to be some kind of power-wave-guide conduits which allow them to work collectively as they perform ship functions. Increase deflector modulation to upper frequency band.
+              </p>
+              <hr>
+              <code>
+                title and subtitle size 2, with medium paragraph (default paragraph size)
+              </code>
+              <h2 class="title is-2">
+                Rhomboid Dronegar 006
+              </h2>
+              <div class="subtitle is-2">
+                Most elementary and valuable statement
+              </div>
+              <p class="is-medium">
+                Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.
+              </p>
+              <hr>
+              <code>
+                title and subtitle size 3, with small paragraph
+              </code>
+              <h3 class="title is-3">
+                The shuttle has no power
+              </h3>
+              <div class="subtitle is-3">
+                K-complex corresponding to an REM state
+              </div>
+              <p class="is-small">
+                It indicates a synchronic distortion in the areas emanating triolic waves. The cerebellum, the cerebral cortex, the brain stem,  the entire nervous system has been depleted of electrochemical energy. Any device like that would produce high levels of triolic waves. These walls have undergone some kind of selective molecular polarization. I haven't determined if our phaser energy can generate a stable field. We could alter the photons with phase discriminators.
+              </p>
+              <hr>
+              <code>
+                title and subtitle size 4, with small paragraph
+              </code>
+              <h4 class="title is-4">
+                EM band when proceeding
+              </h4>
+              <div class="subtitle is-4">
+                Fluctuations in energy readings
+              </div>
+              <p class="is-small">
+                Communication is not possible. The shuttle has no power. Using the gravitational pull of a star to slingshot back in time? We are going to Starbase Montgomery for Engineering consultations prompted by minor read-out anomalies. Probes have recorded unusual levels of geological activity in all five planetary systems. Assemble a team. Look at records of the Drema quadrant. Would these scans detect artificial transmissions as well as natural signals?
+              </p>
+              <hr>
+              <code>Inverting title and subtitle</code>
+              <hr>
+              <code>
+                subtitle and title size 1, with large paragraph
+              </code>
+              <div class="subtitle is-1">
+                Engineering consultations
+              </div>
+              <h1 class="title is-1">
+                Something strange on the detector circuit
+              </h1>
+              <p class="is-large">
+                Deflector power at maximum. Energy discharge in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies. Resistance is futile. Recommend we adjust shield harmonics to the upper EM band when proceeding. These appear to be some kind of power-wave-guide conduits which allow them to work collectively as they perform ship functions. Increase deflector modulation to upper frequency band.
+              </p>
+              <hr>
+              <code>
+                subtitle and title size 2, with medium paragraph (default paragraph size)
+              </code>
+              <div class="subtitle is-2">
+                Rhomboid Dronegar 006
+              </div>
+              <h2 class="title is-2">
+                Most elementary and valuable statement
+              </h2>
+              <p class="is-medium">
+                Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.
+              </p>
+              <hr>
+              <code>
+                subtitle and title size 3, with small paragraph
+              </code>
+              <div class="subtitle is-3">
+                The shuttle has no power
+              </div>
+              <h3 class="title is-3">
+                K-complex corresponding to an REM state
+              </h3>
+              <p class="is-small">
+                It indicates a synchronic distortion in the areas emanating triolic waves. The cerebellum, the cerebral cortex, the brain stem,  the entire nervous system has been depleted of electrochemical energy. Any device like that would produce high levels of triolic waves. These walls have undergone some kind of selective molecular polarization. I haven't determined if our phaser energy can generate a stable field. We could alter the photons with phase discriminators.
+              </p>
+              <hr>
+              <code>
+                subtitle and title size 4, with small paragraph
+              </code>
+              <div class="subtitle is-4">
+                EM band when proceeding
+              </div>
+              <h4 class="title is-4">
+                Fluctuations in energy readings
+              </h4>
+              <p class="is-small">
+                Communication is not possible. The shuttle has no power. Using the gravitational pull of a star to slingshot back in time? We are going to Starbase Montgomery for Engineering consultations prompted by minor read-out anomalies. Probes have recorded unusual levels of geological activity in all five planetary systems. Assemble a team. Look at records of the Drema quadrant. Would these scans detect artificial transmissions as well as natural signals?
+              </p>
+              <hr>
+              <code>
+                Headings that are not titles (no bottom margin)
+              </code>
+              <hr>
+              <code>
+                size 1
+              </code>
+              <h1 class="is-1">
+                Starbase Montgomery
+              </h1>
+              <p class="is-large">
+                We are going to Starbase Montgomery for Engineering consultations prompted by minor read-out anomalies. Probes have recorded unusual levels of geological activity in all five planetary systems.
+              </p>
+              <code>
+                size 2
+              </code>
+              <h2 class="is-2">
+                Molecular polarization
+              </h2>
+              <p>These walls have undergone some kind of selective molecular polarization. I haven't determined if our phaser energy can generate a stable field. We could alter the photons with phase discriminators.</p>
+              <code>
+                size 3
+              </code>
+              <h3 class="is-3">
+                Montgomery for Engineering
+              </h3>
+              <p>We are going to Starbase Montgomery for Engineering consultations prompted by minor read-out anomalies. Probes have recorded unusual levels of geological activity in all five planetary systems.</p>
+              <code>
+                size 4
+              </code>
+              <h4 class="is-4">
+                Alternating Photons
+              </h4>
+              <p class="is-small">
+                These walls have undergone some kind of selective molecular polarization. I haven't determined if our phaser energy can generate a stable field. We could alter the photons with phase discriminators.
+              </p>
+              <code>
+                size 5
+              </code>
+              <h5 class="is-5">
+                Planetary Systems
+              </h5>
+              <p class="is-small">
+                We are going to Starbase Montgomery for Engineering consultations prompted by minor read-out anomalies. Probes have recorded unusual levels of geological activity in all five planetary systems.
+              </p>
+              <code>
+                size 6 (same as 5)
+              </code>
+              <h6 class="is-6">
+                Molecular polarization
+              </h6>
+              <p class="is-small">
+                These walls have undergone some kind of selective molecular polarization. I haven't determined if our phaser energy can generate a stable field. We could alter the photons with phase discriminators.
+              </p>
+              <hr>
+              <code>
+                Lists
+              </code>
+              <hr>
+              <code>
+                Unordered list
+              </code>
+              <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>
+                  Item 3
+                  <ul>
+                    <li>Item 3.1</li>
+                    <li>Item 3.2</li>
+                    <li>Item 3.3</li>
+                  </ul>
+                </li>
+                <li>Item 4</li>
+                <li>
+                  <a href="#">List item link</a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="external-link"
+                  >List item external link</a>
+                </li>
+                <li>Very long list item... with less than 24 hours' reserve power. Shield effectiveness has been reduced 12 percent</li>
+              </ul>
+              <code>
+                Ordered list
+              </code>
+              <ol>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>
+                  Item 3
+                  <ol>
+                    <li>Item 3.1</li>
+                    <li>Item 3.2</li>
+                    <li>Item 3.3</li>
+                  </ol>
+                </li>
+                <li>Item 4</li>
+              </ol>
+              <code>
+                List between paragraphs
+              </code>
+              <p>Resistance is futile. Recommend we adjust shield harmonics to the upper EM band when proceeding. These appear to be some kind of power-wave-guide conduits which allow them to work collectively as they perform ship functions.</p>
+              <ol>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>
+                  Item 3
+                </li>
+                <li>Item 4</li>
+              </ol>
+              <p>Total guidance system failure, with less than 24 hours' reserve power. Shield effectiveness has been reduced 12 percent.</p>
+              <hr>
+              <code>
+                Other elements
+              </code>
+              <hr>
+              <code>
+                blockquote (same styles as the default callout component)
+              </code>
+              <Blockquote>These appear to be some kind of power-wave-guide conduits which allow them to work collectively as they perform ship functions.</Blockquote>
+
+              <code>
+                superscript and subscript
+              </code>
+              <p>These appear<sup>@</sup> to be some kind of power-wave-guide conduits which allow them to work collectively as they perform ship <sub>functions</sub>.</p>
+
+              <code>
+                address
+              </code>
+              <address>
+                1234 Market St.<br>
+                Philadelphia, PA - 19130<br>
+                first.last@phila.gov
+              </address>
+            </div>
+          </div>
+        </div>
+        <hr>
+        <code>
+          default grid columns
+        </code>
+        <div class="highlight-cols">
+          <div
+            v-for="x in 12"
+            :key="`${x}-c`"
+            class="columns"
+          >
+            <div
+              v-for="n in x"
+              :key="n"
+              class="column"
+            >
+              <div>{{ (12 / x).toFixed(1) }}</div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   </div>
@@ -27,4 +350,14 @@ export default {
 
 <style lang="scss">
   @import "./assets/scss/main.scss";
+  .highlight-cols {
+    .column {
+      > div {
+        background-color: $grey;
+        border-radius: 4px;
+        color: $white;
+        text-align: center;
+      }
+    }
+  }
 </style>
