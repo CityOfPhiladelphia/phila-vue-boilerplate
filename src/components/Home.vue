4f9be8260210733af580412e6eb874792d846642
Alejandro Lopez <alejandro.lopez@phila.gov>
2021-07-01 19:25:10 -0400

    adding demo for phone and date components

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,62 @@
 <template>
   <div class="container">
-    Application content...
+    <input-form
+      title="Form title"
+      desc="Form description"
+      :errors="errorCount"
+    >
+      <phone
+        v-model="phoneValue"
+        placeholder="Enter phone number"
+      />
+      <date
+        v-model="dateValue"
+        placeholder="Enter date"
+      />
+      <button
+        slot="submit"
+        class="button is-cta"
+        @click.prevent="submitForm"
+      >
+        Submit
+      </button>
+    </input-form>
   </div>
 </template>
-
 <script>
+import { InputForm, Phone, Date } from '@phila/phila-ui';
 export default {
   name: "Home",
+  components: {
+    InputForm,
+    Phone,
+    Date,
+  },
+  data () {
+    return {
+      phoneValue: '',
+      dateValue: '',
+    };
+  },
+  computed: {
+    errorCount () {
+      let count = 0;
+      return count;
+    },
+  },
+  watch: {
+
+  },
+  mounted() {
+  },
+  methods: {
+    submitForm () {
+      if (this.errorCount === 0) {
+        alert('Form submitted');
+      } else {
+        alert('Please fix the errors first');
+      }
+    },
+  },
 };
 </script>
