39536a6290175a343c2907d96ec159f53b1b7c31
Alejandro Lopez <alejandro.lopez@phila.gov>
2021-07-02 13:15:06 -0400

    moving elements to app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,66 @@
     <app-header />
     <main class="content">
       <router-view />
+
+      <input-form
+        title="Form title"
+        desc="Form description"
+        :errors="errorCount"
+      >
+        <phone
+          v-model="phoneValue"
+          placeholder="Enter phone number"
+        />
+        <date
+          v-model="dateValue"
+          placeholder="Enter date"
+        />
+        <button
+          slot="submit"
+          class="button is-cta"
+          @click.prevent="submitForm"
+        >
+          Submit
+        </button>
+      </input-form>
     </main>
   </div>
 </template>
 
 <script>
-import { AppHeader } from '@phila/phila-ui';
+import { AppHeader, InputForm, Phone, Date } from '@phila/phila-ui';
 
 export default {
   name: 'App',
   components: {
     AppHeader,
+    InputForm,
+    Phone,
+    Date,
   },
   data () {
     return {
+      phoneValue: '',
+      dateValue: '',
     };
+  },
+  computed: {
+    errorCount () {
+      const count = 0;
+      return count;
+    },
+  },
+  methods: {
+    submitForm () {
+      if (this.errorCount === 0) {
+        alert('Form submitted');
+      } else {
+        alert('Please fix the errors first');
+      }
+    },
   },
 };
 </script>
-
 <style lang="scss">
   @import "./assets/scss/main.scss";
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,62 +1,10 @@
 <template>
   <div class="container">
-    <input-form
-      title="Form title"
-      desc="Form description"
-      :errors="errorCount"
-    >
-      <phone
-        v-model="phoneValue"
-        placeholder="Enter phone number"
-      />
-      <date
-        v-model="dateValue"
-        placeholder="Enter date"
-      />
-      <button
-        slot="submit"
-        class="button is-cta"
-        @click.prevent="submitForm"
-      >
-        Submit
-      </button>
-    </input-form>
   </div>
 </template>
 <script>
-import { InputForm, Phone, Date } from '@phila/phila-ui';
+
 export default {
   name: 'Home',
-  components: {
-    InputForm,
-    Phone,
-    Date,
-  },
-  data () {
-    return {
-      phoneValue: '',
-      dateValue: '',
-    };
-  },
-  computed: {
-    errorCount () {
-      const count = 0;
-      return count;
-    },
-  },
-  watch: {
-
-  },
-  mounted () {
-  },
-  methods: {
-    submitForm () {
-      if (this.errorCount === 0) {
-        alert('Form submitted');
-      } else {
-        alert('Please fix the errors first');
-      }
-    },
-  },
 };
 </script>
