a6169fecce47ded46c0090fd3c9d6b1e8d736c00
Daniel Faria <daniel.faria@phila.gov>
2021-07-13 10:33:03 -0400

    testing phone and date field issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,9 @@ module.exports = {
     node: true,
   },
   "extends": [
-    'plugin:vue/essential',
-    '@vue/standard'
+    "eslint:recommended",
+    "plugin:vue/essential",
+    "plugin:vue/recommended",
   ],
   rules: {
     // Disabled only on development
