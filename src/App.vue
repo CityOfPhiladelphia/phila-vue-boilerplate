e09fa1cd6b8b023efbd44d35cef5b78af1ce25da
Daniel Faria <daniel.faria@phila.gov>
2021-08-11 14:49:25 -0400

    building tooltip demo

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,58 @@
     />
     <main>
       <div class="container">
-        Demo code starts here
+        <p>
+          Tooltip position = top-center <tooltip
+            message="Some tooltip text"
+            position="top-center"
+          />
+        </p>
+        <br><br><br><br>
+        <p>
+          Tooltip position = bottom-center <tooltip
+            message="Some tooltip text"
+            position="bottom-center"
+          />
+        </p>
+        <br><br><br><br>
+        <p>
+          Tooltip position = right <tooltip
+            message="Some tooltip text"
+            position="right"
+          />
+        </p>
+        <br><br><br><br>
+        <p>
+          Tooltip position = top-right <tooltip
+            message="Some tooltip text"
+            position="top-right"
+          />
+        </p>
+        <br><br><br><br>
+        <p>
+          Tooltip position = bottom-right <tooltip
+            message="Some tooltip text"
+            position="bottom-right"
+          />
+        </p>
       </div>
     </main>
   </div>
 </template>
 
 <script>
-import { AppHeader } from '@phila/phila-ui';
+
+//modal pass heading or remove it
+//language dropdown // add default languages
+//rework how dropdown works
+//add translation to all components
+
+import { AppHeader, Tooltip } from '@phila/phila-ui';
 
 export default {
   components: {
     AppHeader,
+    Tooltip,
   },
   data () {
     return {};
